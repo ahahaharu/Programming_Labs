3a5ace72bfb44f51d7e684003dca2f6ab95d7b80
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-04-18 00:08:24 +0300

    lab 5 done ig

---
+++ Lab5/Lab5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Lab5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            VES ves = new VES();
+
+            ves.Tariffs.Add(new Tariff { Utility = UtilityType.Water, Rate = 3.5 });
+            ves.Tariffs.Add(new Tariff { Utility = UtilityType.Electricity, Rate = 2.8 });
+            ves.Tariffs.Add(new Tariff { Utility = UtilityType.Gas, Rate = 1.7 });
+
+            Resident resident1 = new Resident { Name = "Богданович" };
+            resident1.ConsumedUtilities.Add(UtilityType.Water, 10);
+            resident1.ConsumedUtilities.Add(UtilityType.Electricity, 15);
+            resident1.ConsumedUtilities.Add(UtilityType.Gas, 12);
+
+            Resident resident2 = new Resident { Name = "Петров" };
+            resident2.ConsumedUtilities.Add(UtilityType.Water, 8);
+            resident2.ConsumedUtilities.Add(UtilityType.Electricity, 20);
+            resident2.ConsumedUtilities.Add(UtilityType.Gas, 10);
+
+            ves.Residents.Add(resident1);
+            ves.Residents.Add(resident2);
+
+            Console.WriteLine("Стоимость всех потребленных услуг жителя " + resident1.Name + ": " + ves.CalculateTotalCost(resident1));
+            Console.WriteLine("Стоимость всех потребленных услуг жителя " + resident2.Name + ": " + ves.CalculateTotalCost(resident2));
+            Console.WriteLine("Стоимость всех оказанных услуг: " + ves.CalculateTotalProvidedServices());
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab5/Lab5/VES.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+
+namespace Lab5
+{
+    public enum UtilityType
+    {
+        Water,
+        Electricity,
+        Gas
+    }
+
+    public class Tariff
+    {
+        public UtilityType Utility { get; set; }
+        public double Rate { get; set; }
+    }
+
+    public class Resident
+    {
+        public string Name { get; set; }
+        public Dictionary<UtilityType, double> ConsumedUtilities { get; set; }
+
+        public Resident()
+        {
+            ConsumedUtilities = new Dictionary<UtilityType, double>();
+        }
+    }
+
+    public class VES
+    {
+        public List<Tariff> Tariffs { get; set; }
+        public List<Resident> Residents { get; set; }
+
+        public VES()
+        {
+            Tariffs = new List<Tariff>();
+            Residents = new List<Resident>();
+        }
+
+        public double CalculateTotalCost(Resident resident)
+        {
+            double totalCost = 0;
+
+            foreach (var utility in resident.ConsumedUtilities)
+            {
+                var tariff = Tariffs.Find(t => t.Utility == utility.Key);
+                totalCost += tariff.Rate * utility.Value;
+            }
+
+            return totalCost;
+        }
+
+        public double CalculateTotalProvidedServices()
+        {
+            double totalCost = 0;
+
+            foreach (var resident in Residents)
+            {
+                totalCost += CalculateTotalCost(resident);
+            }
+
+            return totalCost;
+        }
+    }
+}
