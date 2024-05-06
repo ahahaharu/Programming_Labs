31baf5528e81710499d91f0434b0f9382a73b207
Andrey <112519966+ahahaharu@users.noreply.github.com>
2024-05-06 23:45:48 +0300

    lab 7

---
+++ Lab7/Lab7/Interval.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    public class Interval
+    {
+        private double _start;
+        private double _end;
+
+        public Interval(double start, double end)
+        {
+            if (start > end)
+            {
+                throw new ArgumentException("Начало интервала не может быть больше его конца.");
+            }
+
+            _start = start;
+            _end = end;
+        }
+
+        public double Start
+        {
+            get { return _start; }
+            set
+            {
+                if (value > _end)
+                {
+                    throw new ArgumentException("Начало интервала не может быть больше его конца.");
+                }
+
+                _start = value;
+            }
+        }
+
+        public double End
+        {
+            get { return _end; }
+            set
+            {
+                if (value < _start)
+                {
+                    throw new ArgumentException("Конец интервала не может быть меньше его начала.");
+                }
+
+                _end = value;
+            }
+        }
+
+        public double Length => Math.Abs(_end - _start);
+
+        public static Interval operator +(Interval a, Interval b)
+        {
+            return new Interval(a._start + b._start, a._end + b._end);
+        }
+
+        public static Interval operator -(Interval a, Interval b)
+        {
+            return new Interval(a._start - b._start, a._end - b._end);
+        }
+
+        public static Interval operator *(Interval a, Interval b)
+        {
+            double start = Math.Max(a._start, b._start);
+            double end = Math.Min(a._end, b._end);
+            if (start <= end)
+            {
+                return new Interval(start, end);
+            }
+            else
+            {
+                throw new InvalidOperationException("Intervals do not intersect");
+            }
+        }
+
+        public static Interval operator ++(Interval a)
+        {
+            return new Interval(a._start - 1, a._end + 1);
+        }
+
+        public static Interval operator --(Interval a)
+        {
+            return new Interval(a._start + 1, a._end - 1);
+        }
+
+        public static bool operator ==(Interval a, Interval b)
+        {
+            return a.Length == b.Length;
+        }
+
+        public static bool operator !=(Interval a, Interval b)
+        {
+            return a.Length != b.Length;
+        }
+
+        public static bool operator <(Interval a, Interval b)
+        {
+            return a.Length < b.Length;
+        }
+
+        public static bool operator >(Interval a, Interval b)
+        {
+            return a.Length > b.Length;
+        }
+
+        public static bool operator true(Interval a)
+        {
+            return a.Length != 0;
+        }
+
+        public static bool operator false(Interval a)
+        {
+            return a.Length == 0;
+        }
+
+        public static explicit operator double(Interval a)
+        {
+            return a.Length;
+        }
+
+        public static explicit operator Interval(double length)
+        {
+            return new Interval(0, length);
+        }
+
+        public override string ToString()
+        {
+            return $"Interval from {_start} to {_end}";
+        }
+    }
+
+}
---
+++ Lab7/Lab7/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Interval interval1 = new Interval(1, 5);
+            Interval interval2 = new Interval(3, 7);
+
+            // Демонстрация использования свойства Length
+            Console.WriteLine($"Длина интервала1: {interval1.Length}");
+            Console.WriteLine($"Длина интервала2: {interval2.Length}");
+
+            Console.Write('\n');
+
+            // Демонстрация работы операторов + и -
+            Interval сумма = interval1 + interval2;
+            Interval разность = interval1 - interval2;
+            Console.WriteLine($"Сумма интервалов: {сумма}");
+            Console.WriteLine($"Разность интервалов: {разность}");
+
+            Console.Write('\n');
+
+            // Демонстрация работы оператора *
+            try
+            {
+                Interval пересечение = interval1 * interval2;
+                Console.WriteLine($"Пересечение интервалов: {пересечение}");
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Write('\n');
+
+            // Демонстрация работы операторов ++ и --
+            ++interval1;
+            --interval2;
+            Console.WriteLine($"Расширенный интервал1: {interval1}");
+            Console.WriteLine($"Суженный интервал2: {interval2}");
+
+            Console.Write('\n');
+
+            // Демонстрация работы операторов сравнения
+            Console.WriteLine($"Равны ли интервалы? {interval1 == interval2}");
+            Console.WriteLine($"Меньше ли интервал1, чем интервал2? {interval1 < interval2}");
+
+            Console.Write('\n');
+
+            // Демонстрация работы операторов true и false
+            Console.WriteLine($"Является ли интервал1 непустым? {interval1}");
+            Console.WriteLine($"Является ли интервал2 непустым? {interval2}");
+
+            Console.Write('\n');
+
+            // Демонстрация работы явного преобразования типа
+            double длина1 = (double)interval1;
+            Interval интервал3 = (Interval)длина1;
+            Console.WriteLine($"Длина интервала1: {длина1}");
+            Console.WriteLine($"Новый интервал из длины: {интервал3}");
+        }
+    }
+
+}
